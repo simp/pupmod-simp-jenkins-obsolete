c3c688d66fe436bdaad59c51df1e475cb2712857
Chris Tessmer <chris.tessmer@onyxpoint.com>
2015-10-02 13:35:29 -0400

    (SIMP-519) Fix and modernize spec tests
    Before this commit, 10/13 spec tests in simp-jenkins failed due to a
    missing dependency in .fixtures.yml.  This commit fixes the spec tests
    and updates them to more current testing conventions.
    
    SIMP-519 #comment Fixed failing spec tests in simp-simp
    SIMP-519 #comment Updated spec tests to use simp-rspec-puppet-facts
    SIMP-519 #comment Updated helper files (e.g., Gemfiles, Rakefile)
    SIMP-519 #comment Migrated spec tests' syntax from `should` to `expect`
    SIMP-519 #close
    
    Change-Id: I76364c41092104ba280a8ce8db1d8a7d59a6ddfd

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,17 @@
 require 'pathname'
-require 'rspec-puppet'
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet'
+require 'simp/rspec-puppet-facts'
+include Simp::RspecPuppetFacts
 
 # RSpec Material
+
+def mod_site_pp(content)
+  File.open(@orig_site_pp,'w'){|f| f.write(content) }
+end
+
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+module_name = File.basename(File.expand_path(File.join(__FILE__,'../..')))
 
 # Add fixture lib dirs to LOAD_PATH. Work-around for PUP-3336
 if Puppet.version < "4.0.0"
@@ -27,8 +35,17 @@ default_hiera_config =<<-EOM
   - "default"
 EOM
 
+if not File.directory?(File.join(fixture_path,'hieradata')) then
+  FileUtils.mkdir_p(File.join(fixture_path,'hieradata'))
+end
+
+if not File.directory?(File.join(fixture_path,'modules',module_name)) then
+  FileUtils.mkdir_p(File.join(fixture_path,'modules',module_name))
+end
+
 RSpec.configure do |c|
   c.mock_framework = :rspec
+  c.mock_with :mocha
 
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
@@ -36,17 +53,21 @@ RSpec.configure do |c|
   c.hiera_config = File.join(fixture_path,'hieradata','hiera.yaml')
 
   c.before(:all) do
-    # Add fixture lib dirs to LOAD_PATH. Work-around for PUP-3336
-    if Puppet.version < "4.0.0"
-      Dir["#{fixture_path}/modules/*/lib"].entries.each do |lib_dir|
-        $LOAD_PATH << lib_dir
-      end
-    end
+# Add fixture lib dirs to LOAD_PATH. Work-around for PUP-3336
+if Puppet.version < "4.0.0"
+  Dir["#{fixture_path}/modules/*/lib"].entries.each do |lib_dir|
+    $LOAD_PATH << lib_dir
+  end
+end
+
     data = YAML.load(default_hiera_config)
-    data[:yaml][:datadir] = File.join(fixture_path, 'hieradata')
+    data[:yaml][:datadir] = File.join(fixture_path, 'hieradata').to_s
     File.open(c.hiera_config, 'w') do |f|
       f.write data.to_yaml
     end
+
+    @orig_site_pp = File.join(c.manifest_dir,'site.pp')
+    @orig_site_pp_content = File.read(@orig_site_pp)
   end
 
   c.before(:each) do
@@ -58,6 +79,10 @@ RSpec.configure do |c|
 
   c.after(:each) do
     FileUtils.rm_rf(@spec_global_env_temp)
+  end
+
+  c.after(:all) do
+    mod_site_pp(@orig_site_pp_content)
   end
 end
 
