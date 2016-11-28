7b98469bfc5b3812e68d2286e57bd8a03fccf212
Liz Nemsick <lnemsick.simp@gmail.com>
2016-11-28 15:49:28 -0500

    (SIMP-2123) Fix Travis build for master branches
    SIMP-2129 #close
    SIMP-2123 #comment

--- spec/classes/plugins_spec.rb
+++ spec/classes/plugins_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
 describe 'jenkins::plugins' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
 
-  it { should create_class('jenkins::plugins') }
-  it { should contain_class('rsync') }
-  it { should create_file('/var/lib/jenkins/plugins').with_ensure('directory') }
+        it { should create_class('jenkins::plugins') }
+        it { should contain_class('rsync') }
+        it { should create_file('/var/lib/jenkins/plugins').with_ensure('directory') }
+      end
+    end
+  end
 end
